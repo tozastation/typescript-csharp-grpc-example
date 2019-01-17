1c3caf6809698b155a3b5eca44d50358cfbd8566
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-01-17 14:04:09 +0900

    fix: screenStateをAppの引数に設定

--- client/weather-app/src/App.test.tsx
+++ client/weather-app/src/App.test.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { ScreenStateType } from './states/Screen/ScreenStateType';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App screenState={{screenType: ScreenStateType.LOGIN_SCREEN}}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
